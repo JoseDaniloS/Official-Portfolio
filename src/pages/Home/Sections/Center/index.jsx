56de92e45ef78d0684e87e838eb491ceaac91aae
Jose Danilo <danilojose.1d@gmail.com>
2025-06-19 23:17:39 -0300

    feat: new image background in center section

--- src/pages/Home/Sections/Center/index.jsx
+++ src/pages/Home/Sections/Center/index.jsx
@@ -1,18 +1,18 @@
 import { motion } from "motion/react";
-import BackGround_Lobby from "/Background_Lobby.webp";
+import BackGround_Lobby from "/Background.png";
 import { motionConfig, scrollToSection } from "../../../../utils/functions";
 
 export default function Center() {
   return (
     <section
       id="home"
-      className="w-full relative h-dvh flex bg-cover bg-center max-lg:mt-[88px] flex-col gap-10 text-white justify-center items-center"
+      className="w-full relative h-dvh flex bg-cover bg-center max-lg:mt-[88px] flex-col gap-10  text-white justify-center items-center"
       style={{
         backgroundImage: `url(${BackGround_Lobby})`,
       }}
     >
       <div
-        className="w-full h-full absolute top-0 right-0 bg-[#0E0E0E70]"
+        className="w-full h-full absolute top-0 right-0 bg-[#00000090]"
         aria-hidden="true"
       />
 
@@ -20,9 +20,9 @@ export default function Center() {
         {...motionConfig}
         className="flex relative text-[#999] max-md:w-[90vw] font-bold flex-col max-w-[800px] justify-center items-center gap-2 text-center"
       >
-        <h1 className="text-3xl max-md:text-2xl text-white">Bem-vindo</h1>
+        <h1 className="text-3xl max-md:text-2x">Bem-vindo</h1>
         <h2 className="text-5xl max-md:text-4xl">
-          <span className="text-red-600">Dev. Front-End</span> && CEO da Chronos - Web Soluções
+          <span className="text-red-700">Dev. Front-End</span> && CEO da Chronos - Web Soluções
         </h2>
       </motion.div>
 
