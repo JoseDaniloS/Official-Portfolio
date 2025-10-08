4b69360423afdabfbbf5071f0b0646c2571879f2
JoseDaniloS <danilojose.1d@gmail.com>
2025-10-08 15:24:23 -0300

    feat: new habilities

--- src/database/Habilidades.js
+++ src/database/Habilidades.js
@@ -1,3 +1,4 @@
+import { FaNode } from "react-icons/fa";
 import {
   SiHtml5,
   SiCss3,
@@ -7,70 +8,44 @@ import {
   SiTailwindcss,
   SiPostman,
   SiMongodb,
+  SiExpress,
+  SiStrapi,
+  SiGithub,
+  SiNotion,
+  SiGit,
+  SiDocker,
+  SiAmazonwebservices,
+  SiAmazonroute53,
+  SiAwslambda,
+  SiAwsamplify,
+  SiAmazonapigateway,
+  SiAmazondynamodb,
 } from "react-icons/si";
+import { VscVscode } from "react-icons/vsc";
 
-import { FaAws, FaServer } from "react-icons/fa";
-
-import { MdOutlineDns } from "react-icons/md";
-import { TbApi } from "react-icons/tb";
-import { BiCloudUpload } from "react-icons/bi";
-import { PiDatabaseBold } from "react-icons/pi";
+// Unused imports can be removed, but keeping them doesn't hurt.
 
 export const habilidades = [
-  {
-    nome: "HTML5",
-    icon: SiHtml5,
-  },
-  {
-    nome: "CSS3",
-    icon: SiCss3,
-  },
-  {
-    nome: "JavaScript",
-    icon: SiJavascript,
-  },
-  {
-    nome: "TypeScript",
-    icon: SiTypescript,
-  },
-  {
-    nome: "React.js",
-    icon: SiReact,
-  },
-  {
-    nome: "Tailwind CSS",
-    icon: SiTailwindcss,
-  },
-  {
-    nome: "PostMan",
-    icon: SiPostman,
-  },
-  {
-    nome: "AWS",
-    icon: FaAws,
-  },
-  {
-    nome: "Route53",
-    icon: MdOutlineDns,
-  },
-  {
-    nome: "Lambda",
-    icon: FaServer,
-  },
-  {
-    nome: "Amplify",
-    icon: BiCloudUpload,
-  },
-  {
-    nome: "API Gateway",
-    icon: TbApi,
-  },
-  {
-    nome: "DynamoDB",
-    icon: PiDatabaseBold,
-  },
-  {
-    nome: "MongoDB",
-    icon: SiMongodb,
-  },
+  { nome: "HTML5", icon: SiHtml5 },
+  { nome: "CSS3", icon: SiCss3 },
+  { nome: "JavaScript", icon: SiJavascript },
+  { nome: "TypeScript", icon: SiTypescript },
+  { nome: "React.js", icon: SiReact },
+  { nome: "Tailwind CSS", icon: SiTailwindcss },
+  { nome: "Postman", icon: SiPostman },
+  { nome: "AWS", icon: SiAmazonwebservices }, // ✅ Corrigido para o logo principal da AWS
+  { nome: "Route53", icon: SiAmazonroute53 }, // ✅ Corrigido
+  { nome: "Lambda", icon: SiAwslambda }, // ✅ Corrigido
+  { nome: "Amplify", icon: SiAwsamplify }, // ✅ Corrigido
+  { nome: "API Gateway", icon: SiAmazonapigateway }, // ✅ Corrigido
+  { nome: "DynamoDB", icon: SiAmazondynamodb }, // ✅ Corrigido
+  { nome: "MongoDB", icon: SiMongodb },
+  { nome: "Node.js", icon: FaNode },
+  { nome: "Express.js", icon: SiExpress },
+  { nome: "Strapi", icon: SiStrapi },
+  { nome: "Docker", icon: SiDocker },
+  { nome: "Git", icon: SiGit },
+  { nome: "GitHub", icon: SiGithub },
+  { nome: "VS Code", icon: VscVscode },
+  { nome: "Notion", icon: SiNotion },
 ];
